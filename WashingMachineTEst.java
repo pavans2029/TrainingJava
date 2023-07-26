c9070180a6a3864c74fe2e2e056b0b0463f73f0e
pavans2029 <140367250+pavans2029@users.noreply.github.com>
2023-07-27 01:18:56 +0530

    Add files via upload

--- WashingMachineTEst.java
+++ WashingMachineTEst.java
@@ -1,11 +1,30 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WashingMachineTEst {
 	public static void main(String[] args) {
 		
-	WashingPowder washPowder = new WashingPowder(100, "Nirma", "Front Load", true, 10.0f);
-        Water water = new Water("Tap", 20, "Cold");
+		WashingPowder washPowder = new WashingPowder(100, "Nirma", "Front Load", true, 10.0f);
+		Water water = new Water("soft", 20, "Cold");
+		try {
+			water.setQuantity(10);
+		}
+		catch(WaterNotSufficientException ex1) {
+			System.out.println("Water Issue : "+ex1);
+		}
+		try {
+			water.checkType();
+		}
+		catch(HardWaterException ex2) {
+			System.out.println("Water Issue : "+ex2);
+		}
         Electricity electricity = new Electricity("AC", 220.0f, 8, 2, "PowerCo");
-
+        try {
+        	electricity.checkVoltage();
+		}
+		catch(LowVoltageException ex3) {
+			System.out.println("Voltage Issue : "+ex3);
+		}
         
         Cloth[] clothes = new Cloth[5];
         clothes[0] = new Cloth("Cotton", "White", 5.0f, "Shirt", true);
@@ -15,34 +34,93 @@ public class WashingMachineTEst {
         clothes[4] = new Cloth("Wool", "Grey", 10.0f, "Sweater", true);
 
        
-        WashingMachine washingMachine = new WashingMachine();
+        WashingMachine washingMachine1 = new WashingMachine("washing machine 1");
 
+        try {
+        	Laundry laundryResult = washingMachine1.wash(washPowder, water, electricity, clothes);
+		}
+		catch(ClothesOverloadException ex4) {
+			System.out.println("Overload Issue : "+ex4);
+		}
         
-        Laundry laundryResult = washingMachine.wash(washPowder, water, electricity, clothes);
+        WashingMachine washingMachine2 = new WashingMachine("washing machine 2");
+
+        try {
+        	Laundry laundryResult = washingMachine2.wash(washPowder, water, electricity, clothes);
+		}
+		catch(ClothesOverloadException ex4) {
+			System.out.println("Overload Issue : "+ex4);
+		}
         
-        System.out.println("Laundry Details:");
-        System.out.println("Number of clothes: " + laundryResult.getNumberOfCloths());
-        System.out.println("Time required: " + laundryResult.getTimeRequired() + " hours");
-        System.out.println("Total cost: $" + laundryResult.getTotalCost());
-        System.out.println("Water used: " + laundryResult.getWaterUsed() + " liters");
-        System.out.println("Electricity used: " + laundryResult.getElectricityUsed() + " watts");
-        System.out.println("Cost of washing powder: $" + laundryResult.getCostOfWashingPowder());
+        WashingMachine washingMachine3 = new WashingMachine("washing machine 3");
+
+        try {
+        	Laundry laundryResult = washingMachine3.wash(washPowder, water, electricity, clothes);
+		}
+		catch(ClothesOverloadException ex4) {
+			System.out.println("Overload Issue : "+ex4);
+		}
+        
+//        System.out.println("Laundry Details:");
+//        System.out.println("Number of clothes: " + laundryResult.getNumberOfCloths());
+//        System.out.println("Time required: " + laundryResult.getTimeRequired() + " hours");
+//        System.out.println("Total cost: $" + laundryResult.getTotalCost());
+//        System.out.println("Water used: " + laundryResult.getWaterUsed() + " liters");
+//        System.out.println("Electricity used: " + laundryResult.getElectricityUsed() + " watts");
+//        System.out.println("Cost of washing powder: $" + laundryResult.getCostOfWashingPowder());
 		
 		
 	}
 }
 
-class Machine {
+class WaterNotSufficientException extends Exception //checked
+{
+	WaterNotSufficientException(String msg) {
+		super(msg);
+	}
+}
+class HardWaterException extends Exception //checked
+{
+	HardWaterException(String msg) {
+		super(msg);
+	}
+}
+class LowVoltageException extends Exception{
+	LowVoltageException(String msg){
+		super(msg);
+	}
+}
+class ClothesOverloadException extends Exception{
+	ClothesOverloadException(String msg){
+		super(msg);
+	}
+}
+
+class Machine extends Thread{
 	
 }
 class WashingMachine extends Machine { //isA
+	private String washingMachineName;
 	
+	
+	
+	public WashingMachine(String washingMachineName) {
+	super();
+	this.washingMachineName = washingMachineName;
+}
 	WashingTub washTub = new WashingTub(100,"steel"); //hasA
 
-	
-		Laundry wash(WashingPowder washPowder, Water water, Electricity elect, Cloth cloth[]) {
+		public void run() {
+			for(int i=1; i<=100; i++) {
+				System.out.println(washingMachineName +" progress : "+i+"%");
+			}
+		}
+		Laundry wash(WashingPowder washPowder, Water water, Electricity elect, Cloth cloth[])throws  ClothesOverloadException{
 			
-		int numberOfCloths = cloth.length;
+			int numberOfCloths = cloth.length;
+			if(numberOfCloths > 5) {
+				throw new ClothesOverloadException("More number of clothes");
+			}
 	        float waterUsed = water.getQuantity(); 
 	        float electricityUsed = elect.getUnitUsed() * elect.getVoltage(); 
 
@@ -54,6 +132,7 @@ class WashingMachine extends Machine { //isA
 
 	       
 	        float timeRequired = 1.5f; 
+	        this.start();
 
 	        Laundry laundryResult = new Laundry(numberOfCloths, timeRequired, totalCost, waterUsed, electricityUsed, costOfWashingPowder);
 	        return laundryResult;
@@ -256,6 +335,8 @@ class Water {
 		this.type = type;
 		this.quantity = quantity;
 		this.temperature = temperature;
+		
+		
 	}
 	@Override
 	public String toString() {
@@ -267,11 +348,38 @@ class Water {
 	public void setType(String type) {
 		this.type = type;
 	}
+	public void checkType() throws HardWaterException{
+		String input = type;
+
+        // Regular expression to check if the word "hard" is present
+        String regex = "\\bhard\\b";
+
+        // Create a Pattern object
+        Pattern pattern = Pattern.compile(regex);
+
+        // Create a Matcher object
+        Matcher matcher = pattern.matcher(input);
+
+        // Check if the word "hard" is found in the input string
+        if (matcher.find()) {
+        	throw new HardWaterException("Hard water found");
+            
+        } else {
+            System.out.println("The water is suitable for washing");
+        }
+	}
 	public int getQuantity() {
 		return quantity;
 	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
+	public void setQuantity(int quantity)throws  WaterNotSufficientException {
+		if(quantity > 5 ) {
+			this.quantity = quantity;
+		}
+		else {
+			 WaterNotSufficientException ex = new WaterNotSufficientException("Water quantity less than 5 cubic units");
+			 throw ex;
+		}
+		
 	}
 	public String getTemperature() {
 		return temperature;
@@ -331,7 +439,11 @@ class Electricity {
 	public void setSupplier(String supplier) {
 		this.supplier = supplier;
 	}
-	
+	public void checkVoltage() throws LowVoltageException {
+		if(voltage<200) {
+			throw new LowVoltageException("Low Voltage");
+		}
+	}
 	
 	
 }
